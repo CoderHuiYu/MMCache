203d5095857c60201605b921dc977a17c3d413de
yuhui <yuhui@123>
2021-02-03 16:45:44 +0800

    project inti

---
+++ MMCacheExample/MMCache/MMDiskCache.swift
@@ -0,0 +1,12 @@
+//
+//  MMDiskCache.swift
+//  MMCacheExample
+//
+//  Created by yuhui on 2021/2/3.
+//
+
+import UIKit
+
+class MMDiskCache: NSObject {
+
+}
---
+++ MMCacheExample/MMCacheExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MMCacheExample
+//
+//  Created by yuhui on 2021/2/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
